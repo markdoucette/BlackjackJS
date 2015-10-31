05d7ea7f9a18b89e1d0456ed5be5ca8968911926
Mark Doucette <mark@rhythmtech.ca>
2015-10-31 03:59:22 -0700

    Nightly commit. Update UI, Gamepley Logic

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,13 +1,7 @@
+"use strict"
 /**
  * Blackjack engine written in Javascript. 
  */
-
-/**************************************
- * Game runner
- * @author Mark Doucette
- *************************************/
-
-
 
 /**************************************
  * Card object
@@ -51,19 +45,62 @@ function Card(suit, num) {
     };
 }
 
-/**************************************
- * Deal a card 
- * @return a new Card object
- *************************************/
-var deal = function() {
-    // create a suit from random num between 1 and 4 (4 suits in a deck)
-    var suit = Math.floor(Math.random() * 4) + 1;
-    // create a card number from random num between 1 and 13 (13 cards in a suit)
-    var number = Math.floor(Math.random() * 13) + 1;
-
+/**
+ * Deck Object - Representation of a deck of 52 cards
+ */
+function Deck() { 
+    var deckCards = []; // array of 52 Cards;  
+    
+    // populate the deck with 52 unique cards 
+    for (var i = 1; i <= 4; ++i) { // suits
+       for (var j = 1; j <= 13; ++j) { // numbers
+           deckCards.push(new Card(i, j));
+       } 
+    }
+    
+    /**
+     * @return the array of 52 cards
+     */
+    this.getDeckCards = function() {
+        return deckCards;  
+    };
+    
+    /**
+     * Shuffle the deck using Fisher-Yates algorithm
+     */
+    this.shuffle = function() {
+        var temp; // temporary holder of reference to current card
+        var randCard;
+        for(var i = deckCards.length - 1; i > 0; --i) {
+            temp = deckCards[i]; // store current card 
+            randCard = Math.floor(Math.random() * i); 
+            deckCards[i] = deckCards[randCard];
+            deckCards[randCard] = temp;
+        }   
+    };
+    
+    /**
+     * Deal a card
+     */
+    this.deal = function() {
     // give a new card back
-    return new Card(suit, number);
-};
+        if (deckCards.length > 0) {
+            return deckCards.pop(); // take a card
+        } else {
+            return null;
+        }
+    };
+    
+    /**
+     * Display the deck in the console for debug
+     */
+    this.toString = function() {
+        for (var i = 0; i < deckCards.length; ++i) {
+            console.log("Suit: " + deckCards[i].getSuit()
+                       + ", Number: " + deckCards[i].getNum());
+        }  
+    };
+}
 
 /**************************************
  * Hand object - Representation of a 
@@ -72,17 +109,51 @@ var deal = function() {
 function Hand() {
     var cards = []; // array of players cards
 
-    // populate players hand with 2 cards to start
-    cards.push(deal());
-    cards.push(deal());
-
+    /**
+     * Add a card to the hand
+     */
+    this.hit = function(card) {
+        cards.push(card);         
+    };
+    
     /**
      * @return the array of players cards
      */
     this.getCards = function() {
         return cards;
     };
+    
+    /**
+     * Display the deck in the console for debug
+     */
+    this.toString = function() {
+        for (var i = 0; i < cards.length; ++i) {
+            console.log("Suit: " + cards[i].getSuit()
+                       + ", Number: " + cards[i].getNum());
+        }  
+    };
 }
+
+/**************************************
+ * Game runner
+ * @author Mark Doucette
+ *************************************/
+var deck;
+var playerHand;
+var dealerHand;
+
+function PlayBlackJack() {
+    deck = new Deck();
+    deck.shuffle();
+    playerHand = new Hand();
+    playerHand.hit(deck.deal());
+    playerHand.hit(deck.deal());
+    dealerHand = new Hand();
+    dealerHand.hit(deck.deal());
+    dealerHand.hit(deck.deal());
+        
+    
+};
 
 
 
